759b24c394aa8d4db67d50d1ad314db42f8df1a2
Leal <gabryellows@gmail.com>
2023-09-04 20:53:02 -0300

    criação de um servidor em python para devolver a pagina index

---
+++ build-dev/run.py
@@ -0,0 +1,26 @@
+import logging
+import http.server
+import socketserver
+import getpass
+
+class MyHTTPHandler(http.server.SimpleHTTPRequestHandler):
+    def log_message(selfm format, *args):
+        logging.info("%s - - [%s] %s\n" % (
+            self.client_address[0],
+            self.log_date_time_string(),
+            format%args
+        ))
+
+logging.basicConfig(
+    filename='/log/http-server.log',
+    format='%(asctime)s - %(levelname)s - %(message)s',
+    level=logging.INFO
+)
+logging.getLogger().addHandler(logging.StreamHandler())
+logging.info('Starting server...')
+PORT = 8000
+
+httpd = socketserver.TCPServer(("", PORT), MyHTTPHandler)
+logging.info('Server started on port %s' % PORT)
+logging.info('Username: %s' % getpass.getuser())
+httpd.serve_forever()
