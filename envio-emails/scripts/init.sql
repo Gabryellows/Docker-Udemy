54d47365b021f73d9a6dfcf87977710e136eb480
Leal <gabryellows@gmail.com>
2023-09-12 22:22:44 -0300

    criacao dos networks para web, db, app

--- envio-emails/scripts/init.sql
+++ envio-emails/scripts/init.sql
@@ -6,5 +6,5 @@ create table emails (
     id serial not null,
     data timestamp not null default current_timestamp,
     assunto varchar(100) not null,
-    mensagem varchar(255) not null,
+    mensagem varchar(255) not null
 );
